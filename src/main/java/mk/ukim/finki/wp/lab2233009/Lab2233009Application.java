31ab49ba64f29d1a40cef2c471754a80959fc6d2
Mario Spasovski <mario.spaska@gmail.com>
2026-04-16 16:47:37 +0200

    Lab 2 , task 6

--- src/main/java/mk/ukim/finki/wp/lab2233009/Lab2233009Application.java
+++ src/main/java/mk/ukim/finki/wp/lab2233009/Lab2233009Application.java
@@ -2,8 +2,10 @@ package mk.ukim.finki.wp.lab2233009;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Lab2233009Application {
 
     public static void main(String[] args) {
